190cf2472bca88de8374a9d2672ecd60fefdb061
churtado-gh <carlos.hurtado@outlook.es>
2024-02-16 00:17:15 +0100

    Cambio de nombre y reestructurar función

--- R/CentralityFunctions.R
+++ R/CentralityFunctions.R
@@ -2,41 +2,53 @@
 
 # PH Indices ------------------------------------------------------------
 
-#' Title
+#' Calculate Presence and Hierarchy Indices
 #'
-#' @description To obtain the presence (P, frequency of occurrence) and hierarchy
-#' (H, influence on others) of constructs associated with an implication grid.
+#' This function computes the presence (P, frequency of occurrence) and
+#' hierarchy (H, influence on others) indices for constructs within an implication grid.
+#' It can standardize these indices based on the maximum degree if required.
 #'
-#' @param wimp
+#' @param wimp An object of class 'wimp', which contains an implication grid
+#'   and associated constructs.
+#' @param method A character string specifying the method used to calculate
+#'   the degree indices. Default is "simple". Other methods may be available
+#'   depending on the implementation of 'degree_index' function.
+#' @param std A logical value indicating whether to standardize the P and H
+#'   indices based on the maximum total degree of the constructs. Defaults to TRUE.
 #'
-#' @param method
+#' @return A 2-column matrix with the presence index 'p' and the hierarchy index 'h'
+#'   for each construct. If 'std' is TRUE, these values are standardized.
 #'
-#' @return
 #' @export
 #'
 #' @examples
+#' # Assuming 'wimp' is a predefined object with an implication grid
+#' ph_indices <- ph_index(wimp)
+#' ph_indices_std <- ph_index(wimp, std = TRUE)
+#' ph_indices_non_std <- ph_index(wimp, std = FALSE)
 #'
 
-index_ph <- function(wimp, method = "simple"){
+ph_index <- function(wimp, method = "simple", std = TRUE){
 
   # Connectivity of constructs
   c.io <- degree_index(wimp, method = method)
 
-  # Linear transformation matrix
-  coef <- 0.5 * sqrt(2)
-  mat.proy <- matrix(c(coef, coef, -coef, coef), nrow = 2, byrow = TRUE)
-
+  # Rearrange In - Out columns
+  c.io <- c[, c(2,1,3)]
   # Extract In and Out columns
   in.out <- c.io[, 1:2]
 
   # Calculate p and h for each row
-  p <- rowSums(in.out * c(coef, coef))
-  h <- rowSums(in.out * c(-coef, coef))
+  coef <- 0.5 * sqrt(2)
+  p <- rowSums(c(coef, coef) * in.out)
+  h <- rowSums(c(-coef, coef) * in.out)
 
   # Standardization
-  max.total.deg <- max(c.io[, 3])
-  p <- p / max.total.deg
-  h <- h / max.total.deg
+  if (std){
+    max.total.deg <- max(c.io[, 3])
+    p <- p / max.total.deg
+    h <- h / max.total.deg
+  }
 
   # Create the new matrix with p and h dimensions
   ph.mat <- matrix(c(p, h), ncol = 2, byrow = FALSE,
