82fdb8043306915f40cebccedc075262586d6bd9
MaiteBenitez <imaite.benitez@gmail.com>
2024-03-05 17:53:19 +0000

    actualización nombre función

--- R/RtCFunctions.R
+++ R/RtCFunctions.R
@@ -1,25 +1,26 @@
 ## RESISTANCE TO CHANGE FUNCTIONS ##
 
-# consyretr ---------------------------------------------------------------
+# cq.fb ---------------------------------------------------------------
 
-#' Calculation of consequence and feedback for each construct
+#' Calculation of consequence and feedback effects for each construct of the
+#' system
 #'
-#' @description This function calculates the number of consequences and feebacks
-#' for each construct to analyze resistance to change
+#' @description This function calculates the numbers of consequences and feedbacks
+#' for each construct to analyze its resistance to change
 #'
 #' @param wimp
 #' @param std
 #'
-#' @return Returns a dataframe with number of consequences and feedbacks
+#' @return Returns a dataframe with numbers of consequences and feedbacks
 #'
 #' @import
 #'
 #' @export
 #'
 #' @examples
-#' cyr_index (example.wimp)
+#' cq.fb_index (example.wimp)
 #'
-cyr_index <- function(wimp, std=FALSE) {
+cq.fb_index <- function(wimp, std=FALSE) {
   wimp <- .align.wimp(wimp)
   wmatrix<- wimp $scores $weights
   consequence <- apply (wmatrix, MARGIN = 1, FUN = sum)
